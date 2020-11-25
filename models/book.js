55a2b024db4cd9ace0def47f73a5cea73f0860f8
Lisa McDonald <nixxxxer@aol.com>
2020-11-24 22:25:33 -0500

    add schema

--- models/book.js
+++ models/book.js
@@ -2,10 +2,35 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+
+  id:  { 
+    type: String, 
+    required: true 
+  },
+  
+  title: { 
+    type: String, 
+    required: true 
+  },
+
+  authors: [{ 
+    type: String, 
+    required: true 
+  }],
+
+  description: {
+    type: String,
+    required: true 
+  },
+
+  image: {
+    type: String
+  },
+
+  link: { 
+    type: String
+  }
+
 });
 
 const Book = mongoose.model("Book", bookSchema);
