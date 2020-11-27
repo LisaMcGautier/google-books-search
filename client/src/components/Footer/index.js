4aa600fb636265887e0704c0ca4f1c45428c853f
Lisa McDonald <nixxxxer@aol.com>
2020-11-27 15:38:23 -0500

    ADD link to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,9 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-      <span>Powered by Google Books API</span>
+      <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" />
+      <span><a target="_blank" rel="noopener noreferrer" href="https://developers.google.com/books/docs/overview"
+      style={{ fontWeight: "bold", color: "#f5eddb" }}>Powered by Google Books API</a></span>
     </footer>
   );
 }
