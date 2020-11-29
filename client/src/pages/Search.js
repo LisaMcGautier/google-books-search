629051aee83b52287ebb871b8d05c4f32f72fc30
Lisa McDonald <nixxxxer@aol.com>
2020-11-29 10:59:24 -0500

    render book results

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -37,17 +37,21 @@ function Books() {
     API.searchGoogle(query)
       .then(response => {
         // console.log(response)
-        // console.log(response.data)
-        console.log(response.data.items)
-        //console.log(response.data.items[0])
-        console.log(response.data.items[0].id)
-        //console.log(response.data.items[0].volumeInfo)
-        console.log(response.data.items[0].volumeInfo.title)
-        console.log(response.data.items[0].volumeInfo.subtitle)
-        console.log(response.data.items[0].volumeInfo.authors)
-        console.log(response.data.items[0].volumeInfo.description)
-        console.log(response.data.items[0].volumeInfo.imageLinks.smallThumbnail)
-        console.log(response.data.items[0].volumeInfo.infoLink)
+        setBooks(response.data.items)
+
+
+        // // console.log(response.data)
+        // console.log(response.data.items)
+        // //console.log(response.data.items[0])
+        // // LOGS Google Books :ID
+        // console.log(response.data.items[0].id)
+        // //console.log(response.data.items[0].volumeInfo)
+        // console.log(response.data.items[0].volumeInfo.title)
+        // console.log(response.data.items[0].volumeInfo.subtitle)
+        // console.log(response.data.items[0].volumeInfo.authors)
+        // console.log(response.data.items[0].volumeInfo.description)
+        // console.log(response.data.items[0].volumeInfo.imageLinks.smallThumbnail)
+        // console.log(response.data.items[0].volumeInfo.infoLink)
       })
 
 
@@ -106,6 +110,10 @@ function Books() {
       .catch(err => console.log(err));
   }
 
+  
+  function visitBookPage(index) {
+    window.open(books[index].volumeInfo.infoLink, "_blank");
+  }
 
 
 
@@ -160,44 +168,48 @@ function Books() {
               <Col size="md-12">
                 {/* <Col size="md-10 md-offset-1"> */}
 
-                <div className="card m-3 p-3">
-                  <Row>
-                    <Col size="md-6">
-                      {/* <h5 className="card-title">Card title</h5> */}
-                      <h4 className="card-title">BOOK title</h4>
-                      <h6 className="card-title">book subtitle</h6>
-                      <h5 className="card-title">Author name(s)</h5>
-                    </Col>
+                {books ? books.map((book, index) => {
+                  return (
+                    <div className="card m-3 p-3" key={book.volumeInfo.title}>
+                      <Row>
+                        <Col size="md-6">
+                          {/* <h5 className="card-title">Card title</h5> */}
+                          <h4 className="card-title">{book.volumeInfo.title}</h4>
+                          <h6 className="card-title">{book.volumeInfo.subtitle ? book.volumeInfo.subtitle : ""}</h6>
+                          <h5 className="card-title">{book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : ""}</h5>
+                        </Col>
 
-                    <Col size="md-6">
-                      <button style={{ float: "right", marginBottom: 10 }} className="btn btn-primary shadow m-3">View</button>
-                      <button style={{ float: "right", marginBottom: 10 }} className="btn btn-success shadow m-3">Save</button>
-                    </Col>
-                  </Row>
+                        <Col size="md-6">
+                          <button onClick={() => visitBookPage(index)} style={{ float: "right", marginBottom: 10 }} className="btn btn-primary shadow m-3">View</button>
+                          <button style={{ float: "right", marginBottom: 10 }} className="btn btn-success shadow m-3">Save</button>
+                        </Col>
+                      </Row>
 
-                  {/* <div className="card mb-3" style={{ maxWidth: "540px" }}> */}
-                  <div className="row no-gutters">
-                    <div className="col-md-1 m-3">
-                      {/* <div className="col-md-1"> */}
-                      <img src="http://books.google.com/books/content?id=6TyCDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api" className="card-img" alt="test" />
-                    </div>
-                    <div className="col-md-8">
-                      <div className="card-body">
+                      {/* <div className="card mb-3" style={{ maxWidth: "540px" }}> */}
+                      <div className="row no-gutters">
+                        <div className="col-md-1 m-3">
+                          {/* <div className="col-md-1"> */}
+                          <img src={book.volumeInfo.imageLinks.smallThumbnail} className="card-img" alt="book cover" />
+                        </div>
+                        <div className="col-md-8">
+                          <div className="card-body">
 
-                        {/* <h5 className="card-title">Card title</h5> */}
-                        {/* <button className="btn btn-warning shadow m-3">View</button>
+                            {/* <h5 className="card-title">Card title</h5> */}
+                            {/* <button className="btn btn-warning shadow m-3">View</button>
                         <button className="btn btn-primary shadow m-3">Save</button> */}
 
-                        {/* <button style={{ float: "right", marginBottom: 10 }} className="btn btn-warning shadow m-3">View</button>
+                            {/* <button style={{ float: "right", marginBottom: 10 }} className="btn btn-warning shadow m-3">View</button>
                         <button style={{ float: "right", marginBottom: 10 }} className="btn btn-primary shadow m-3">Save</button> */}
 
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p>Lorem impsum It beggared all description.  more in sorrow than in anger.  He has eaten me out of house and home.  Let me love him for that; and do you love him because I do. Look, here comes the Duke. The sixth age shifts Into the lean and slipper'd pantaloon, With spectacles on nose and pouch on side, His youthful hose, well sav'd, a world too wide For his shrunk shank; and his big manly voice, Turning again toward childish treble, pipes And whistles in his sound.  But look to it: Find out thy brother wheresoe'er he is; Seek him with candle; bring him dead or living Within this twelvemonth, or turn thou no more To seek a living in our territory.   Sir, I am a true labourer: I earn that I eat, get that I wear; owe no man hate, envy no man's happiness; glad of other men's good, content with my harm; and the greatest of my pride is to see my ewes graze and my lambs.</p>
-                        {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
+                            <p className="card-text">{book.volumeInfo.description}</p>
+                            {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                </div>
+                    </div>)
+                }) : <h3>No Results to Display</h3>}
+
+
 
                 {/* <article>
                   <h1>Synopsis</h1>
@@ -211,7 +223,7 @@ function Books() {
             </Row>
           </Results>
 
-          {books.length ? (
+          {/* {books.length ? (
             <List>
               {books.map(book => (
                 <ListItem key={book._id}>
@@ -226,7 +238,7 @@ function Books() {
             </List>
           ) : (
               <h3>No Results to Display</h3>
-            )}
+            )} */}
         </Col>
       </Row>
 
